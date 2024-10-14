e27f836eb4a7274b3f3e82a1521e047dc4208aaa
Stnikan <stnikan2016@gmail.com>
2024-10-14 11:52:46 +0300

    lab3 filter

--- lab3/kod1.py
+++ lab3/kod1.py
@@ -8,7 +8,7 @@ def nothing(x):
 
 pathImg1 = "./lab3/3-1.PNG"
 pathImg2 = "./lab3/3-2.PNG"
-winName = "Test window"
+winName = "Border"
 
 cv2.namedWindow(winName, cv2.WINDOW_GUI_NORMAL)
 img1 = cv2.imread(
@@ -58,13 +58,15 @@ while (1):
 
 
 cv2.destroyWindow(winName)
+winName = "Blur"
+
 cv2.namedWindow(winName, cv2.WINDOW_GUI_NORMAL)
 
 cv2.createTrackbar("size_H", winName, 0, 200, nothing)
 cv2.createTrackbar("size_W", winName, 0, 200, nothing)
 cv2.createTrackbar("anchor_H", winName, 0, 200, nothing)
 cv2.createTrackbar("anchor_W", winName, 0, 200, nothing)
-cv2.createTrackbar("border", winName, 0, len(All_border)-1, nothing)
+cv2.createTrackbar("border", winName, 0, len(All_border)-2, nothing)
 
 
 while (1):
@@ -88,36 +90,114 @@ while (1):
     if key == 27:
         break
 
+cv2.destroyWindow(winName)
+winName = "BoxFilter"
+
+cv2.namedWindow(winName, cv2.WINDOW_GUI_NORMAL)
+
+cv2.createTrackbar("ddepth", winName, 0, 200, nothing)
+cv2.createTrackbar("size", winName, 0, 200, nothing)
+cv2.createTrackbar("normalize", winName, 0, 1, nothing)
+cv2.createTrackbar("border", winName, 0, len(All_border)-1, nothing)
 
 
-"""
-img_new = cv2.boxFilter(
-    img,  # входное изображение
-    ddepth,  # глубина изображения-результата
-    ksize,  # размер ядра
-    anchor,  # положение якорной точки
-    normalize,  # нормирование (если True)
-    bType  # тип рамки
+while (1):
+    ddepth = cv2.getTrackbarPos("ddepth", winName)
+    size = cv2.getTrackbarPos("size", winName)+2
+    f = cv2.getTrackbarPos("normalize", winName)
+    border = cv2.getTrackbarPos("border", winName)
+    
+    
+    img_new = cv2.boxFilter(
+    img1,  # входное изображение
+    -1,  # глубина изображения-результата
+    (size,size),  # размер ядра
+    (size//2,size//2),  # положение якорной точки
+    normalize=f,  # нормирование (если True)
+    borderType=All_border[border],   # тип рамки
 )
+    cv2.imshow(winName, img_new)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
 
-img_new = cv2.medianBlur(
-    img,  # входное изображение
-    ksize  # размер ядра
-)
+cv2.destroyWindow(winName)
+winName = "medianBlur"
+cv2.namedWindow(winName, cv2.WINDOW_GUI_NORMAL)
 
-img_new = cv2.GaussianBlur(
-    img,  # входное изображение
-    ksize,  # размер ядра
+
+cv2.createTrackbar("size", winName, 0, 100, nothing)
+
+while (1):
+    size = cv2.getTrackbarPos("size", winName)
+    
+    
+    img_new = cv2.medianBlur(
+    img1,  # входное изображение
+    sizeH*2+1  # размер ядра
+)   
+    
+    cv2.imshow(winName, img_new)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cv2.destroyWindow(winName)
+winName = "GaussianBlur"
+cv2.namedWindow(winName, cv2.WINDOW_GUI_NORMAL)
+
+
+cv2.createTrackbar("size", winName, 0, 100, nothing)
+cv2.createTrackbar("sigmaX", winName, 0, 200, nothing)
+cv2.createTrackbar("sigmaY", winName, 0, 200, nothing)
+cv2.createTrackbar("border", winName, 0, len(All_border)-1, nothing)
+
+while (1):
+    size = cv2.getTrackbarPos("size", winName)
+    sigmaX = cv2.getTrackbarPos("sigmaX", winName)
+    sigmaY = cv2.getTrackbarPos("sigmaY", winName)
+    border = cv2.getTrackbarPos("border", winName)
+    
+    img_new = cv2.GaussianBlur(
+    img1,  # входное изображение
+    (size*2+1,size*2+1),  # размер ядра
     sigmaX,  # сигма по оси X
     sigmaY,  # сигма по оси Y
-    bType  # тип рамки
+    borderType=All_border[border]  # тип рамки
 )
+    
+    cv2.imshow(winName, img_new)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
 
-img_new = cv2. bilateralFilter(
-    img,  # входное изображение
-    d,  # размер окрестности пикселя
+cv2.destroyWindow(winName)
+winName = "bilateralFilter"
+cv2.namedWindow(winName, cv2.WINDOW_GUI_NORMAL)
+
+
+cv2.createTrackbar("size", winName, 0, 100, nothing)
+cv2.createTrackbar("sigmaColor", winName, 0, 200, nothing)
+cv2.createTrackbar("sigmaSpace", winName, 0, 200, nothing)
+cv2.createTrackbar("border", winName, 0, len(All_border)-1, nothing)
+
+while (1):
+    size = cv2.getTrackbarPos("size", winName)
+    sigmaColor = cv2.getTrackbarPos("sigmaColor", winName)
+    sigmaSpace = cv2.getTrackbarPos("sigmaSpace", winName)
+    border = cv2.getTrackbarPos("border", winName)
+    
+    img_new = cv2. bilateralFilter(
+    img1,  # входное изображение
+    size,  # размер окрестности пикселя
     sigmaColor,  # ширина второго компонента весовой функции
     sigmaSpace,  # ширина первого компонента весовой функции
-    bType  # тип рамки
+    borderType=All_border[border]  # тип рамки
 )
-"""
+    
+    cv2.imshow(winName, img_new)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+
+
